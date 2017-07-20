bc8037e1ee1ec832f012ba53a57529f6dbf525fa
陈正荣 <rick@chenzhengrongdeMacBook-Pro.local>
2017-07-21 03:36:22 +0800

    add 3 tables and add response interceptors to show errormsg

--- server/app/middleware/error_handler.js
+++ server/app/middleware/error_handler.js
@@ -3,8 +3,12 @@ module.exports = () => {
     try {
       yield next;
     } catch (err) {
-      this.app.emit('error', err, this);
+      this.app.emit('error', err, this);//后台打印出错误的堆栈信息
+      console.log(2222222222)
+      console.log(err)
+      console.log(333333333)
       const status = err.status || 500;
+      // const success = false
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
       const error = status === 500 && this.app.config.env === 'prod'
         ? 'Internal Server Error'
